4b0ad31856ecddf9aa098f11174968a23ddd4743
M1tchM355 <mitchell.messerly@gmail.com>
2025-05-01 15:05:03 -0600

    Added equals and hashCode to ChessBoard, ChessMove, ChessPiece, and ChessPosition

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -66,5 +69,22 @@ public class ChessBoard {
         for (int i = 1; i < 9; i++){
             squares[7][i] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(squares, that.squares);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(squares);
     }
 }
