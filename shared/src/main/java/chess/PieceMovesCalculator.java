cdd6fc9824773b26b5ac753d7f47b202d57eb33d
M1tchM355 <mitchell.messerly@gmail.com>
2025-04-30 19:45:45 -0600

    Added recommended move calculators

---
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -0,0 +1,13 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class PieceMovesCalculator {
+
+    public PieceMovesCalculator(){}
+
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        return new ArrayList<>();
+    }
+}
