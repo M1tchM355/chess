07ee0e1d01b579f39ae2fd514a5866f848720b44
M1tchM355 <mitchell.messerly@gmail.com>
2025-05-03 16:30:21 -0600

    Added pawn promotion and capture!

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -31,10 +31,28 @@ public class PawnMovesCalculator extends PieceMovesCalculator{
             }
             //Take diagonally
             if(col-1>0 && row+1<9 && board.getPiece(new ChessPosition(row+1,col-1))!=null && board.getPiece(new ChessPosition(row+1,col-1)).getTeamColor()!=pieceColor){
-                moves.add(new ChessMove(myPosition, new ChessPosition(row+1,col-1),null));
+                //Promote
+                if(row==7){
+                    for(ChessPiece.PieceType type : ChessPiece.PieceType.values()){
+                        if(type!= ChessPiece.PieceType.PAWN && type!= ChessPiece.PieceType.KING){
+                            moves.add(new ChessMove(myPosition, new ChessPosition(row + 1, col - 1), type));
+                        }
+                    }
+                } else {
+                    moves.add(new ChessMove(myPosition, new ChessPosition(row + 1, col - 1), null));
+                }
             }
             if(col+1<9 && row+1<9 && board.getPiece(new ChessPosition(row+1,col+1))!=null && board.getPiece(new ChessPosition(row+1,col+1)).getTeamColor()!=pieceColor){
-                moves.add(new ChessMove(myPosition, new ChessPosition(row+1,col+1),null));
+                //Promote
+                if(row==7){
+                    for(ChessPiece.PieceType type : ChessPiece.PieceType.values()){
+                        if(type!= ChessPiece.PieceType.PAWN && type!= ChessPiece.PieceType.KING){
+                            moves.add(new ChessMove(myPosition, new ChessPosition(row + 1, col + 1), type));
+                        }
+                    }
+                } else {
+                    moves.add(new ChessMove(myPosition, new ChessPosition(row + 1, col + 1), null));
+                }
             }
         //Black pawns
         } else {
@@ -58,10 +76,28 @@ public class PawnMovesCalculator extends PieceMovesCalculator{
             }
             //Take diagonally
             if(col-1>0 && row-1>0 && board.getPiece(new ChessPosition(row-1,col-1))!=null && board.getPiece(new ChessPosition(row-1,col-1)).getTeamColor()!=pieceColor){
-                moves.add(new ChessMove(myPosition, new ChessPosition(row-1,col-1),null));
+                //Promote
+                if(row==2){
+                    for(ChessPiece.PieceType type : ChessPiece.PieceType.values()){
+                        if(type!= ChessPiece.PieceType.PAWN && type!= ChessPiece.PieceType.KING){
+                            moves.add(new ChessMove(myPosition, new ChessPosition(row - 1, col - 1), type));
+                        }
+                    }
+                } else {
+                    moves.add(new ChessMove(myPosition, new ChessPosition(row - 1, col - 1), null));
+                }
             }
             if(col+1<9 && row-1>0 && board.getPiece(new ChessPosition(row-1,col+1))!=null && board.getPiece(new ChessPosition(row-1,col+1)).getTeamColor()!=pieceColor){
+                //Promote
+                if(row==2){
+                    for(ChessPiece.PieceType type : ChessPiece.PieceType.values()){
+                        if(type!= ChessPiece.PieceType.PAWN && type!= ChessPiece.PieceType.KING){
+                            moves.add(new ChessMove(myPosition, new ChessPosition(row - 1, col + 1), type));
+                        }
+                    }
+                } else {
                 moves.add(new ChessMove(myPosition, new ChessPosition(row-1,col+1),null));
+                }
             }
         }
         return moves;
