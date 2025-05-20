dd0e5720f8191b548bd9e210b99878620c970685
M1tchM355 <mitchell.messerly@gmail.com>
2025-05-20 13:42:31 -0600

    Implemented register methods in MemoryUserDAO and RegisterService, plus created AlreadyTakenException

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,4 +1,27 @@
 package dataaccess;
 
+import model.AuthData;
+import model.UserData;
+
+import java.util.HashMap;
+import java.util.UUID;
+
 public class MemoryUserDAO implements UserDAO{
+    final static private HashMap<String, UserData> users = new HashMap<>();
+
+    public static UserData getUser(String username) throws DataAccessException{
+        UserData user = users.get(username);
+        if(user == null){
+            throw new DataAccessException("User does not exist");
+        }
+        return user;
+    }
+
+    public static void createUser(UserData user){
+        users.put(user.username(),user);
+    }
+
+    public static AuthData createAuth(UserData user){
+        return new AuthData(UUID.randomUUID().toString(),user.username());
+    }
 }
--- server/src/main/java/service/RegisterService.java
+++ server/src/main/java/service/RegisterService.java
@@ -1,4 +1,22 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryUserDAO;
+import model.AuthData;
+import model.UserData;
+import request.RegisterRequest;
+import result.RegisterResult;
+
 public class RegisterService extends ChessService{
+    public RegisterResult register(RegisterRequest req) throws AlreadyTakenException{
+        try{
+            UserData userCheck = MemoryUserDAO.getUser(req.username());
+            throw new AlreadyTakenException("Username is already taken");
+        } catch (DataAccessException e) {
+            UserData user = new UserData(req.username(),req.password(),req.email());
+            MemoryUserDAO.createUser(user);
+            AuthData auth = MemoryUserDAO.createAuth(user);
+            return new RegisterResult(auth.username(), auth.authToken());
+        }
+    }
 }
