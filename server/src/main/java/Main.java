9165aa4a8e006443e0848adf74e4baecb725b6fa
M1tchM355 <mitchell.messerly@gmail.com>
2025-05-20 14:01:44 -0600

    server Main.java now creates each of the three DAOs

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,10 +1,16 @@
 import chess.*;
+import dataaccess.*;
 import server.Server;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+
+        UserDAO userDAO = new MemoryUserDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        GameDAO gameDAO = new MemoryGameDAO();
+
         Server mainServer = new Server();
         mainServer.run(8080);
     }
