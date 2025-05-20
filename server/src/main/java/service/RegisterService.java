aadaface1cb9861ac927857f2ab4bc3e603dcf96
M1tchM355 <mitchell.messerly@gmail.com>
2025-05-20 14:12:37 -0600

    RegisterService keeps track of the DAOs it needs and has register()

--- server/src/main/java/service/RegisterService.java
+++ server/src/main/java/service/RegisterService.java
@@ -1,21 +1,30 @@
 package service;
 
+import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
-import dataaccess.MemoryUserDAO;
+import dataaccess.UserDAO;
 import model.AuthData;
 import model.UserData;
 import request.RegisterRequest;
 import result.RegisterResult;
 
 public class RegisterService extends ChessService{
+    private final UserDAO userDAO;
+    private final AuthDAO authDAO;
+
+    public RegisterService(UserDAO userDAO, AuthDAO authDAO){
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
+    }
+
     public RegisterResult register(RegisterRequest req) throws AlreadyTakenException{
         try{
-            UserData userCheck = MemoryUserDAO.getUser(req.username());
+            UserData userCheck = userDAO.getUser(req.username());
             throw new AlreadyTakenException("Username is already taken");
         } catch (DataAccessException e) {
             UserData user = new UserData(req.username(),req.password(),req.email());
-            MemoryUserDAO.createUser(user);
-            AuthData auth = MemoryUserDAO.createAuth(user);
+            userDAO.createUser(user);
+            AuthData auth = authDAO.createAuth(user);
             return new RegisterResult(auth.username(), auth.authToken());
         }
     }
