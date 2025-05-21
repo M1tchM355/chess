d74187f565dcc4a397cc3ec1aea1cbd29e74ec01
M1tchM355 <mitchell.messerly@gmail.com>
2025-05-21 14:08:55 -0600

    Implemented MemoryGameDAO

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,13 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.GameData;
+
+import java.util.ArrayList;
+
 public interface GameDAO {
+    public int createGame(String gameName);
+    public GameData getGame(int gameID) throws DataAccessException;
+    public ArrayList<GameData> listGames();
+    public GameData updateGame(int gameID, ChessGame.TeamColor playerColor, String username) throws DataAccessException;
 }
--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,4 +1,50 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.GameData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MemoryGameDAO implements GameDAO{
+    final private HashMap<Integer, GameData> games = new HashMap<>();
+    private int nextID = 1;
+
+    @Override
+    public int createGame(String gameName) {
+        games.put(nextID, new GameData(nextID,"","",gameName,new ChessGame()));
+        nextID++;
+        return nextID-1;
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        GameData game = games.get(gameID);
+        if(game==null){
+            throw new DataAccessException("Invalid gameID");
+        }
+        return game;
+    }
+
+    @Override
+    public ArrayList<GameData> listGames() {
+        ArrayList<GameData> gameList = new ArrayList<>();
+        return null;
+    }
+
+    @Override
+    public GameData updateGame(int gameID, ChessGame.TeamColor playerColor, String username) throws DataAccessException {
+        GameData oldGame = games.get(gameID);
+        if(oldGame==null){
+            throw new DataAccessException("Invalid gameID");
+        }
+        GameData newGame;
+        if(playerColor == ChessGame.TeamColor.WHITE){
+            newGame = new GameData(gameID, username, oldGame.blackUsername(), oldGame.gameName(), oldGame.game());
+        } else {
+            newGame = new GameData(gameID, oldGame.whiteUsername(), username, oldGame.gameName(), oldGame.game());
+        }
+        games.replace(gameID,newGame);
+        return newGame;
+    }
 }
