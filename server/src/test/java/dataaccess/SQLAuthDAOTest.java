9bf4e39fad3a5d50a1f69ffd08313cd946ca7da1
M1tchM355 <mitchell.messerly@gmail.com>
2025-05-28 14:23:38 -0600

    Added all successful tests for SQLAuthDAO

--- server/src/test/java/dataaccess/SQLAuthDAOTest.java
+++ server/src/test/java/dataaccess/SQLAuthDAOTest.java
@@ -7,9 +7,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mindrot.jbcrypt.BCrypt;
 
+import java.util.UUID;
+
 import static java.sql.Statement.RETURN_GENERATED_KEYS;
 
 public class SQLAuthDAOTest {
+    private final String username = "TestUser";
+    private final String password = "testPassword";
+    private final String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+    private final String email = "email";
+    private final String authToken = UUID.randomUUID().toString();
+
     @BeforeEach
     public void setup() {
         try (var conn = DatabaseManager.getConnection()) {
@@ -23,9 +31,6 @@ public class SQLAuthDAOTest {
 
     @Test
     public void createAuthSuccessTest() {
-        String username = "TestUser";
-        String hashedPassword = BCrypt.hashpw("testPassword", BCrypt.gensalt());
-        String email = "email";
         UserData user = new UserData(username, hashedPassword, email);
         try {
             AuthData returnedAuth = new SQLAuthDAO().createAuth(user);
@@ -45,6 +50,77 @@ public class SQLAuthDAOTest {
                 }
             }
         } catch (Exception e){
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    public void getAuthSuccessTest() {
+        String statement = "INSERT INTO auth (username, authToken) VALUES (?, ?)";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1,username);
+                ps.setString(2,authToken);
+
+                ps.executeUpdate();
+            }
+
+            AuthData returnedAuth = new SQLAuthDAO().getAuth(authToken);
+            Assertions.assertEquals(username,returnedAuth.username());
+            Assertions.assertEquals(authToken,returnedAuth.authToken());
+        } catch (Exception e) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    public void deleteAuthSuccessTest() {
+        String statement = "INSERT INTO auth (username, authToken) VALUES (?, ?)";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1,username);
+                ps.setString(2,authToken);
+
+                ps.executeUpdate();
+            }
+
+            new SQLAuthDAO().deleteAuth(new AuthData(authToken,username));
+
+            try (var ps = conn.prepareStatement("SELECT * FROM auth", RETURN_GENERATED_KEYS)) {
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        Assertions.fail();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    public void clearAuthsTest() {
+        try {
+            var statement1 = "INSERT INTO auth (username, authToken) VALUES (?, ?)";
+            var statement2 = "SELECT * FROM auth";
+            try (var conn = DatabaseManager.getConnection()) {
+                try (var ps = conn.prepareStatement(statement1)) {
+                    ps.setString(1,username);
+                    ps.setString(2,authToken);
+
+                    ps.executeUpdate();
+                }
+
+                new SQLAuthDAO().clearAuths();
+                try (var ps = conn.prepareStatement(statement2, RETURN_GENERATED_KEYS)) {
+                    try (var rs = ps.executeQuery()) {
+                        if (rs.next()) {
+                            Assertions.fail();
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
             Assertions.fail();
         }
     }
