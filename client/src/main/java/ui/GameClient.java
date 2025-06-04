bce67beb478658db011a97e267fc0b06e9debc6d
M1tchM355 <mitchell.messerly@gmail.com>
2025-06-03 21:09:31 -0600

    Added eval and help to GameClient

--- client/src/main/java/ui/GameClient.java
+++ client/src/main/java/ui/GameClient.java
@@ -1,7 +1,30 @@
 package ui;
 
+import java.util.Arrays;
+
 public class GameClient extends Client {
     public GameClient(String serverURL){
         super(serverURL);
+    }
+
+    @Override
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String help(){
+        return """
+                quit - quit application
+                help - see possible commands
+                """;
     }
 }
