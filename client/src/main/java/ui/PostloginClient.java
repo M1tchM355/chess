47ba39840c4b324ab3d8a9429e58e9535f650806
M1tchM355 <mitchell.messerly@gmail.com>
2025-06-04 15:29:47 -0600

    Implemented list in PostloginClient. Added a hashmap in the client that keeps track of the numbers assigned to each game and their IDs to join them later

--- client/src/main/java/ui/PostloginClient.java
+++ client/src/main/java/ui/PostloginClient.java
@@ -1,19 +1,27 @@
 package ui;
 
+import model.GameData;
 import request.CreateGameRequest;
 import request.JoinGameRequest;
+import request.ListGamesRequest;
 import request.LogoutRequest;
 import result.CreateGameResult;
 import result.JoinGameResult;
+import result.ListGamesResult;
 import result.LogoutResult;
 import server.ResponseException;
 
 import java.sql.Array;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
 
 public class PostloginClient extends Client {
+    public HashMap<Integer, Integer> gameMap;
+
     public PostloginClient(String serverURL){
         super(serverURL);
+        gameMap = new HashMap<>();
     }
 
     @Override
@@ -22,8 +30,6 @@ public class PostloginClient extends Client {
             var tokens = input.toLowerCase().split(" ");
             var cmd = (tokens.length > 0) ? tokens[0] : "help";
             var params = Arrays.copyOfRange(tokens, 1, tokens.length);
-            //var params = Arrays.copyOf(paramsNoAuth, paramsNoAuth.length + 1);
-            //params[params.length - 1] = this.authToken;
             return switch (cmd) {
                 case "create" -> create(params);
                 case "list" -> list(params);
@@ -60,12 +66,31 @@ public class PostloginClient extends Client {
     }
 
     public String list(String... params) throws ResponseException {
-        return "";
+        ListGamesRequest request = new ListGamesRequest(authToken);
+        ListGamesResult result = this.server.listGames(request);
+        Collection<GameData> games = result.games();
+        StringBuilder ret = new StringBuilder();
+        int counter = 1;
+        for (GameData game : games) {
+            gameMap.put(counter, game.gameID());
+            ret.append(counter)
+                    .append("\nName: ").append(game.gameName())
+                    .append("\nWhite player: ").append(game.whiteUsername())
+                    .append("\nBlack player: ").append(game.blackUsername());
+        }
+        return ret.toString();
     }
 
     public String join(String... params) throws ResponseException {
         if (params.length == 2) {
-            int ID = Integer.parseInt(params[0]);
+            if (gameMap.isEmpty()) {
+                throw new ResponseException(400, "You must first list the games");
+            }
+            int num = Integer.parseInt(params[0]);
+            Integer ID = gameMap.get(num);
+            if (ID == null) {
+                throw new ResponseException(400, "Invalid ID");
+            }
             String color = params[1];
             JoinGameRequest request = new JoinGameRequest(color, ID, authToken);
             this.server.joinGame(request);
