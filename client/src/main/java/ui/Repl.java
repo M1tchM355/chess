3bef24269c53884b283cef6c4a51ee37aacc4b0c
M1tchM355 <mitchell.messerly@gmail.com>
2025-06-03 20:45:51 -0600

    Created initial repl and clients from petshop

---
+++ client/src/main/java/ui/Repl.java
@@ -0,0 +1,44 @@
+package ui;
+
+import java.util.Scanner;
+import static ui.EscapeSequences.*;
+
+public class Repl {
+    private final PreloginClient preloginClient;
+    private final PostloginClient postloginClient;
+    private final GameClient gameClient;
+    private final Client activeClient;
+
+    public Repl(String serverUrl) {
+        preloginClient = new PreloginClient(serverUrl, this);
+        postloginClient = new PostloginClient();
+        gameClient = new GameClient();
+        activeClient = preloginClient;
+    }
+
+    public void run() {
+        System.out.println("\uD83D\uDC36 Welcome to chess! Sign in to start.");
+        System.out.print(preloginClient.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = activeClient.eval(line);
+                System.out.print(SET_TEXT_COLOR_BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n>>> " + SET_TEXT_COLOR_GREEN);
+    }
+
+}
