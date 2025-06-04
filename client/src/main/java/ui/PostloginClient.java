f010daec545874e60ae0390669e7ed28029e5f50
M1tchM355 <mitchell.messerly@gmail.com>
2025-06-03 21:06:58 -0600

    Created help for PostloginClient

--- client/src/main/java/ui/PostloginClient.java
+++ client/src/main/java/ui/PostloginClient.java
@@ -4,4 +4,16 @@ public class PostloginClient extends Client {
     public PostloginClient(String serverURL){
         super(serverURL);
     }
+
+    public String help(){
+        return """
+                create <NAME> - create a chess game with a name
+                list - list all games
+                join <ID> [WHITE|BLACK] - join game as specified color
+                observe <ID> - observe game
+                logout - logs you out
+                quit - quit application
+                help - see possible commands
+                """;
+    }
 }
