01c6413ba7ab3a2d59f285c37893f8e7b0b2a60e
M1tchM355 <mitchell.messerly@gmail.com>
2025-06-09 15:50:39 -0600

    Added all eval switch options and relating functions

--- client/src/main/java/ui/GameClient.java
+++ client/src/main/java/ui/GameClient.java
@@ -19,6 +19,12 @@ public class GameClient extends Client {
             var cmd = (tokens.length > 0) ? tokens[0] : "help";
             var params = Arrays.copyOfRange(tokens, 1, tokens.length);
             return switch (cmd) {
+                case "redraw" -> draw();
+                case "move" -> move(params);
+                case "highlight" -> highlight(params);
+                case "resign" -> resign();
+                case "leave" -> leave();
+                case "quit" -> quit();
                 default -> help();
             };
         } catch (ResponseException ex) {
@@ -28,8 +34,37 @@ public class GameClient extends Client {
 
     public String help() throws ResponseException {
         return """
+                redraw - redraw the chess board
+                move <START> <END> - move the piece at start position to the end position. Valid positions are a letter and number (like a7)
+                highlight <POSITION> - highlight all legal moves for the piece at position. Valid positions are a letter and number (like a7)
+                resign - resign the game
+                leave - leave the chess game
                 quit - quit application
                 help - see possible commands
                 """;
+    }
+
+    public String leave() {
+        return "Left game";
+    }
+
+    public String draw() {
+        return null;
+    }
+
+    public String move(String... params) {
+        return null;
+    }
+
+    public String highlight(String... params) {
+        return null;
+    }
+
+    public String resign() {
+        return null;
+    }
+
+    public String quit() {
+        return "quit";
     }
 }
