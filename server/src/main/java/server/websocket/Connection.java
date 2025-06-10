2534a388286e69e034585dfeb155480f25d860fd
M1tchM355 <mitchell.messerly@gmail.com>
2025-06-10 12:55:08 -0600

    Connections now have the username, gameID, and session

--- server/src/main/java/server/websocket/Connection.java
+++ server/src/main/java/server/websocket/Connection.java
@@ -5,12 +5,14 @@ import org.eclipse.jetty.websocket.api.Session;
 import java.io.IOException;
 
 public class Connection {
-    public String visitorName;
+    public String username;
+    public Integer gameID;
     public Session session;
 
-    public Connection(String visitorName, Session session) {
-        this.visitorName = visitorName;
+    public Connection(String username, Integer gameID, Session session) {
+        this.username = username;
         this.session = session;
+        this.gameID = gameID;
     }
 
     public void send(String msg) throws IOException {
