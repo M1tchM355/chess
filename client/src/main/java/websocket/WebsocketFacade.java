cff13770f33c66c89ab6a7d82184ee2a91be80f4
M1tchM355 <mitchell.messerly@gmail.com>
2025-06-10 12:12:48 -0600

    Created WebsocketFacade constructor

--- client/src/main/java/websocket/WebsocketFacade.java
+++ client/src/main/java/websocket/WebsocketFacade.java
@@ -1,11 +1,37 @@
 package websocket;
 
-import javax.websocket.CloseReason;
-import javax.websocket.Endpoint;
-import javax.websocket.EndpointConfig;
-import javax.websocket.Session;
+import com.google.gson.Gson;
+import server.ResponseException;
+import websocket.messages.ServerMessage;
+
+import javax.websocket.*;
+import java.net.URI;
 
 public class WebsocketFacade extends Endpoint {
+    Session session;
+    ServerMessageObserver observer;
+
+    public WebsocketFacade(String url, ServerMessageObserver observer) throws ResponseException {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/ws");
+            this.observer = observer;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    ServerMessage notification = new Gson().fromJson(message, ServerMessage.class);
+                    observer.notify(notification);
+                }
+            });
+        } catch (Exception e) {
+            throw new ResponseException(500, e.getMessage());
+        }
+    }
+
     @Override
     public void onClose(Session session, CloseReason closeReason) {
         super.onClose(session, closeReason);
